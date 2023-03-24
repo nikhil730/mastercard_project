5315e6359f453faa48f7b3efaa6f49ca2b01fefd
Rahul Jha <rahujha04@gmail.com>
2023-03-24 17:37:35 +0530

    rosolved the error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Nav from "./components/Nav/Nav"
 import Map from "./components/Map/Map";
 import List from "./components/List/List";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
