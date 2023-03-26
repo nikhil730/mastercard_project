b4213dbf3a128dca631386bb111055a76b09daa6
nikhil730 <nikhilbhalla196@gmail.com>
2023-03-26 16:14:25 +0530

    added a button on decription page.

--- src/components/DescriptionPage/DescriptionPage.jsx
+++ src/components/DescriptionPage/DescriptionPage.jsx
@@ -3,12 +3,18 @@ import './description.css'
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 const DescriptionPage = () => {
+  const history=useHistory();
   const location=useLocation();
   const [locationstate,setLocationstate]=React.useState({name: "", description: "", lat: 0,lng: 0})
 
-
+  const handleclick=()=>{
+    history.push("/");
+  }
   React.useEffect(()=>{
     console.log(location);
 
@@ -21,6 +27,7 @@ const DescriptionPage = () => {
   //console.log(props);
   return (
     <div className="description">
+       <Button onClick={handleclick} className="button">Return</Button>
        <div className="description__image">
         <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/India_geo_stub.svg" alt="india.png" />
        </div>
