e4e0b552fa0ffb636e1e7a68f7fc1600a9fce178
Rahul Jha <rahujha04@gmail.com>
2023-04-09 20:08:31 +0530

    added prev and next buttons

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import './pages.css';
 
 const Pages = () => {
   const location = useLocation();
   const prop = location.state;
-  // console.log(prop.heading);
+
+  const history = useHistory();
+
+  const handlePrevious = () =>{
+    history.push('/secondPage');
+  }
+
+  const handleNext = () =>{
+    history.push('/map');
+  }
+
   return (
     <div className="pages">
       <h1>{prop.heading}</h1>
       <p>{prop.content}</p>
+      <div className="buttons">
+        <button className="previous" onClick={handlePrevious}>Previous</button>
+        <button className="next" onClick={handleNext}>Next</button>
+      </div>
     </div>
   )
 }
 
-export default Pages
+export default Pages;
--- src/components/SecondPage/SecondPage.jsx
+++ src/components/SecondPage/SecondPage.jsx
@@ -5,49 +5,69 @@ import { useHistory } from 'react-router-dom';
 const SecondPage = () => {
     const [texts, setTexts] = useState([
         {
-            heading: "Targetted marketig",
-            content: "The campaign should highlight MasterStore's high-end products and emphasize the unique experience that customers can have by shopping at their stores The campaign can be promoted on social media platforms like WeChat, Weibo, and Douyin, which are popular in China."
+            heading: 'Targetted marketig',
+            content:
+                "The campaign should highlight MasterStore's high-end products and emphasize the unique experience that customers can have by shopping at their stores The campaign can be promoted on social media platforms like WeChat, Weibo, and Douyin, which are popular in China.",
         },
         {
-            heading: "Collaboration with travel agencies",
-            content: "To create customized travel packages for Chinese tourists that include a shopping experience at MasterStore. This can help attract more Chinese tourists to the store and increase sales revenue."
+            heading: 'Collaboration with travel agencies',
+            content:
+                'To create customized travel packages for Chinese tourists that include a shopping experience at MasterStore. This can help attract more Chinese tourists to the store and increase sales revenue.',
         },
         {
-            heading: "Strategically located stores",
-            content: "Consider opening new stores in strategic locations in China to capture the growing demand for luxury goods. This can help the company expand its presence in the Chinese market and increase revenue from Chinese consumers."
+            heading: 'Strategically located stores',
+            content:
+                'Consider opening new stores in strategic locations in China to capture the growing demand for luxury goods. This can help the company expand its presence in the Chinese market and increase revenue from Chinese consumers.',
         },
         {
-            heading: "Customer experience",
-            content: "Invest in improving the customer experience at MasterStore by hiring bilingual staff and providing Chinese language signage and product information. This can help create a more welcoming environment for Chinese customers and improve their shopping experience."
-        }
+            heading: 'Customer experience',
+            content:
+                'Invest in improving the customer experience at MasterStore by hiring bilingual staff and providing Chinese language signage and product information. This can help create a more welcoming environment for Chinese customers and improve their shopping experience.',
+        },
     ]);
 
     const history = useHistory();
 
-    const handleClick = (event) => {
-        const myText = event;
+    const handleClick = (index) => {
+        const myText = texts[index];
         history.push({
             pathname: '/pages',
             state: {
-                heading: myText.heading, 
+                heading: myText.heading,
                 content: myText.content,
             },
         });
-    }
+    };
 
+    const handlePrev = () => {
+        history.push('/');
+    };
+
+    const handleNext = () => {
+        history.push('/map');
+    };
 
     return (
         <div className="secondPage">
             <h1>Our Solution</h1>
             <div className="secondpage__buttons">
-                {texts.map((text) => (
-                    <button key={text.heading} onClick={() => handleClick(text)}>{text.heading}</button>
+                <button onClick={handlePrev}>
+                    Previous
+                </button>
+                {texts.map((text, index) => (
+                    <button
+                        key={text.heading}
+                        onClick={() => handleClick(index)}
+                    >
+                        {text.heading}
+                    </button>
                 ))}
+                <button
+                    onClick={handleNext}
+                >
+                    Next
+                </button>
             </div>
-            {/* <div className="secondpage__prevnext">
-                <button onClick={navigateToPrev}>Prev</button>
-                <button onClick={navigateToNext}>Next</button>
-            </div> */}
         </div>
     );
 };
