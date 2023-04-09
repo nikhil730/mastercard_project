582bc2412ec99ea13b66df730cef5c43f6ebe5b1
Rahul Jha <rahujha04@gmail.com>
2023-04-09 17:25:38 +0530

    new pages added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Nav from "./components/Nav/Nav";
 import Map from "./components/Map/Map";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import DescriptionPage from "./components/DescriptionPage/DescriptionPage";
+import Start from "./components/Start/Start";
+import SecondPage from "./components/SecondPage/SecondPage";
+import Pages from "./components/Pages/Pages";
 
 function App() {
   return (
     <Router>
-      <Nav />
       <Switch>
         <Route exact path="/">
+          <Start/>
+        </Route>
+        <Route exact path="/map">
+          <Nav />
           <center>
             <Map />
           </center>
         </Route>
         <Route exact path="/description">
           <DescriptionPage />
+        </Route>
+        <Route exact path="/secondPage">
+          <SecondPage/>
+        </Route>
+        <Route exact path="/pages">
+          <Pages/>
         </Route>
       </Switch>
     </Router>
---
+++ src/components/Pages/Pages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pages = ({props}) => {
+
+  console.log(props);
+  return (
+    <div className="pages">
+
+    </div>
+  )
+}
+
+export default Pages
